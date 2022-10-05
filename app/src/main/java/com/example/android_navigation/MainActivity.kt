601dee560d88a3c9d5358f5afa04016bb902099e
Jjol_twi <hgymin3129@gmail.com>
2022-10-05 10:19:44 +0900

    add binding&&config project build.gradle

--- app/src/main/java/com/example/android_navigation/MainActivity.kt
+++ app/src/main/java/com/example/android_navigation/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.android_navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_navigation.databinding.ActivityMainBinding
+
+private lateinit var mbinding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mbinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mbinding.root)
     }
 }
